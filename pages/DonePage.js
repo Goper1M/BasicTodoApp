821600ae7f2ce0880d2d45a19edfe5a7cbbc891f
Spoon <mhuynh321@gmail.com>
2019-09-29 21:18:31 -0400

    - get data from parent - displayed completed task on new screen & updated state

--- pages/DonePage.js
+++ pages/DonePage.js
@@ -1,22 +1,63 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import TodoItem from '../components/TodoItems';
 
 export default class DonePage extends React.Component{
     static navigationOptions = {
         title: 'Completed'
     }
-    constructor () {
-        super ();
+    constructor (props) {
+        super (props);
+
+        this.state= {
+            receivedTodos: this.props.navigation.getParam ('passedTodos', 'temp')
+        }
+        // console.log(receivedTodos);
     }
 
+    unComplete = (itemId) => {
+
+        let receivedTodos = this.state.receivedTodos;
+        receivedTodos.forEach(mtodo => {
+            if (itemId === mtodo.id && mtodo.done){
+                mtodo.done = false;
+            }
+        })
+        this.setState({receivedTodos})
+    }
+
+    
+
     render(){
+        // const mdata = props.receivedTodos;
+        // console.log(mdata);
+        // let receivedTodos = this.props.navigation.getParam ('passedTodos', 'temp');
+        // console.log(receivedTodos);
         return(
             <View style = { style.container }>
-                <Text>New Page!</Text>
+                {/* <Text></Text> */}
+                <FlatList
+                    data={this.state.receivedTodos}
+                    extraData= {this.state}
+                    keyExtractor={( item, index ) => index.toString()}
+                    renderItem={({ item, index }) => {
+                        if (item.done){
+                            return (
+                                <TodoItem
+                                    todoItem={item}
+                                    completed={(itemId) => this.unComplete(itemId)}
+                                />
+                            )
+                        }
+                    }}
+                />
             </View>
         );
     };
-};
+
+}
 
 
 const style = StyleSheet.create({
